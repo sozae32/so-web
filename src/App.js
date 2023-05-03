1b8d4fc4e9d3cdb121916ef08cdc7e73baefce0e
sozae32 <admin@studioosmunda.com>
2023-05-03 16:01:33 -0700

    bug fixes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coming Soon!
+          Edit <code>src/App.js</code> and save to reload. GitHub Auto 3:49!
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
